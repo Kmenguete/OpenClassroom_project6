bf1526df3e523c7e6a4a5740f1a8219a459b4fd3
Kmenguete <menguetekevin@gmail.com>
2022-01-12 14:50:42 +0100

    creating the js file that let to open the modal window

--- just_stream_it.js
+++ just_stream_it.js
@@ -20,4 +20,12 @@ button.onclick = function click() {
 
 span.onclick = function close() {
 	modal.style.display = "none";
+} 
+
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = function event() {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 }
