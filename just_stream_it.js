91ee4d0de8593dcd42d7fb8be9b56a01ca23677d
Kmenguete <menguetekevin@gmail.com>
2022-01-12 14:33:45 +0100

    creating the js file that let to open the modal window

---
+++ just_stream_it.js
@@ -0,0 +1,11 @@
+// Get the modal
+
+var modal = document.getElementById("modal_window");
+
+// Get the button that opens the modal
+
+var button = document.getElementByClassName("modal_info");
+
+// Get the <span> element that closes the modal
+
+var span = document.getElementByClassName("close")[0];
