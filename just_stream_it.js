a97678305367dcdf66c3b6102a4cfb25a5ad7c86
Kmenguete <menguetekevin@gmail.com>
2022-01-13 13:41:36 +0100

    I successfully opened my modal window

--- just_stream_it.js
+++ just_stream_it.js
@@ -3,12 +3,12 @@ var modal = document.getElementById('modal_window');
 // Get open modal button
 var buttonBestMovieInfo = document.getElementById('the_best_movie_info');
 // Get close button
-var closeButton = document.getElementByClassName('close');
+var closeButton = document.getElementById('close');
 
 // Listen for click
-buttonBestMovieInfo.addEventListener('click', openModal);
+buttonBestMovieInfo.addEventListener('click', openBestMovieModal);
 
 // Function to open modal
-function openModal(){
+function openBestMovieModal(){
 	modal.style.display = 'block';
 }
