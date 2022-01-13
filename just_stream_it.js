8ae9249b8dd51fbebbf77823a269b75cc2e89d7c
Kmenguete <menguetekevin@gmail.com>
2022-01-13 13:56:05 +0100

    I successfully closed my modal window

--- just_stream_it.js
+++ just_stream_it.js
@@ -3,12 +3,19 @@ var modal = document.getElementById('modal_window');
 // Get open modal button
 var buttonBestMovieInfo = document.getElementById('the_best_movie_info');
 // Get close button
-var closeButton = document.getElementById('close');
+var closeBestMovieInfoButton = document.getElementById('close');
 
-// Listen for click
+// Listen for open click
 buttonBestMovieInfo.addEventListener('click', openBestMovieModal);
+// Listen for close click
+closeBestMovieInfoButton.addEventListener('click', closeBestMovieModal);
 
 // Function to open modal
 function openBestMovieModal(){
 	modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeBestMovieModal(){
+	modal.style.display = 'none';
 }
