ee605dc736e319d17c95c492f27ee4dcfa2949e9
Kmenguete <menguetekevin@gmail.com>
2022-01-24 15:48:46 +0100

    I am trying to load the best movie from the API to my web page

--- just_stream_it.js
+++ just_stream_it.js
@@ -1,8 +1,29 @@
+// API url for the movie with the highest imdb score
 
-// API URL 
+let bestMovie = 'http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=';
 
-const apiUrl = 'http://localhost:8000/api/v1/titles/';
+// Defining async function
+async function getApi(url) {
+	// Storing response 
+	const response = await fetch(url);
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getApi(bestMovie);
 
-fetch(apiUrl)
-  .then(res => res.json())
-  .then(data => console.log(data))
+//Function to hide the loader
+function hideloader() {
+	document.getElementById('loading').style.display = 'none';
+} 
+// Function to define innerHTML for HTML tage
+function showBestMovie(data) {
+	let movieImage = '/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=/image_url'
+} 
+// Setting innerHTML as movieImage variable
+document.getElementById('best_movie').innerHTML = movieImage;
