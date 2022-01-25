28c27d1414fbbb10dc9919e0b99618df1a46ce48
Kmenguete <menguetekevin@gmail.com>
2022-01-25 17:45:23 +0100

    Trying to extract the best movie data from the API and to insert it into the html

--- just_stream_it.js
+++ just_stream_it.js
@@ -1,29 +1,8 @@
-// API url for the movie with the highest imdb score
+let bestMovie = fetch('http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=');
 
-let bestMovie = 'http://localhost:8000/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=';
+bestMovie.then(async (responseData)=>{
+    console.loge(responseData);
 
-// Defining async function
-async function getApi(url) {
-	// Storing response 
-	const response = await fetch(url);
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getApi(bestMovie);
-
-//Function to hide the loader
-function hideloader() {
-	document.getElementById('loading').style.display = 'none';
-} 
-// Function to define innerHTML for HTML tage
-function showBestMovie(data) {
-	let movieImage = '/api/v1/titles/?actor=&actor_contains=&company=&company_contains=&country=&country_contains=&director=&director_contains=&genre=&genre_contains=&imdb_score=&imdb_score_max=&imdb_score_min=&lang=&lang_contains=&max_year=&min_year=&rating=&rating_contains=&sort_by=-imdb_score&title=&title_contains=&writer=&writer_contains=&year=/image_url'
-} 
-// Setting innerHTML as movieImage variable
-document.getElementById('best_movie').innerHTML = movieImage;
+    const response = await responseData.json();
+    console.log(response);
+});
