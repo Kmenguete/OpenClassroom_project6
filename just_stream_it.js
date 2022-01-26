af24831c5ad5cf48df98a8a360d4f8ec2c12be30
Kmenguete <menguetekevin@gmail.com>
2022-01-26 10:35:32 +0100

    Trying to retrieve data from the response 200 API in my web page(the console)

--- just_stream_it.js
+++ just_stream_it.js
@@ -1,8 +1,27 @@
-let bestMovie = fetch('http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=');
+let bestMovie = fetch('http://localhost:8000/api/v1/titles/1508669');
 
 bestMovie.then(async (responseData)=>{
     console.loge(responseData);
 
     const response = await responseData.json();
-    console.log(response);
+    console.log(response); 
+
+    try{
+
+    // Get best movie data from API
+
+    
+    const title = response.title;
+    const gender = response.genres;
+    const releaseDate = response.year;
+    const rated = response.rated;
+
+
+    } catch(error) {
+    	console.log(error);
+    }
+}) 
+
+.catch((error) => {
+	console.log(error);
 });
