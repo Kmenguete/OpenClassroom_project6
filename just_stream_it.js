7d5ca7d3c1c9250b93f57dacf58af2b26febadfa
Kmenguete <menguetekevin@gmail.com>
2022-01-31 15:49:01 +0100

    I want to iterate over my modal window. In other words, for each li, I would like to create a modal window dynamicallyusing javascript

--- just_stream_it.js
+++ just_stream_it.js
@@ -108,9 +108,65 @@ searchbestMovies.then(async (responseData)=>{
 
 		    	const listItem = document.createElement('li');
 
-		    	// Add the item text 
-		    	
+		    	// Add the item content 
+
 		    	const bestMoviesData = fetch(bestMovies[i]);
+		    	bestMoviesData.then(async (responseData)=>{
+		    		console.log(responseData);
+
+		    		const response = await responseData.json();
+		    		console.log(response); 
+		    		try{
+
+    // Get best movies data from API
+
+    
+    const title = response.title;
+    const gender = response.genres;
+    const releaseDate = response.year;
+    const rated = response.rated;
+    const imdbScore = response.imdb_score;
+    const filmDirector = response.directors;
+    const listOfActors = response.actors;
+    const duration = response.duration;
+    const country = response.countries;
+    const movieAbstract = response.description; 
+    const movieImage = response.image_url;
+
+    // Get data displayed
+
+    const display_title = document.querySelector("#title_best_movies");
+    const display_gender = document.querySelector("#gender_best_movies");
+    const display_releaseDate = document.querySelector("#release_date_best_movies");
+    const display_rated = document.querySelector("#rated_best_movies");
+    const display_imdbScore = document.querySelector("#imdb_score_best_movies");
+    const display_filmDirector = document.querySelector("#film_director_best_movies");
+    const display_listOfActors = document.querySelector("#actors_best_movies");
+    const display_duration = document.querySelector("#duration_best_movies");
+    const display_country = document.querySelector("#country_best_movies");
+    const display_movieAbstract = document.querySelector("#movie_abstract_best_movies"); 
+    const bestMovieImage = `<img src="${movieImage}"/>`;
+    const display_movieImage = document.querySelector("#best_movies_images"); 
+
+
+    display_title.innerHTML = "Movie: " + title;
+    display_gender.innerHTML = "Gender: " + gender;
+    display_releaseDate.innerHTML = "Release Date: " + releaseDate;
+    display_rated.innerHTML = "Rated: " + rated;
+    display_imdbScore.innerHTML = "Imdb score: " + imdbScore;
+    display_filmDirector.innerHTML = "Movie director: " + filmDirector;
+    display_listOfActors.innerHTML = "List of Actors: " + listOfActors;
+    display_duration.innerHTML = "Duration: " + duration;
+    display_country.innerHTML = "Country: " + country;
+    display_movieAbstract.innerHTML = "Movie abstract: " + movieAbstract;
+    display_moviesImage.insertAdjacentHTML("afterbegin", bestMovieImage);
+
+
+    } catch(error) {
+    	console.log(error);
+    }
+
+		    	})
 
 		    	// Add listItem to the listElement
 
