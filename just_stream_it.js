0019288d56af2f3abd1e4c4b57757ddc5109b4b5
Kmenguete <menguetekevin@gmail.com>
2022-02-15 14:31:29 +0100

    fetching missing best movies

--- just_stream_it.js
+++ just_stream_it.js
@@ -228,7 +228,16 @@ searchbestMoviesPage2.then(async (responseData)=>{
 	try{
 		function RetrieveMissingBestMovies(){
 			// Establish the array that stores the responses from the API
-			var bestMoviesPage2 = [response.results[0].url, response.results[1].url, response.results[2].url],
+			var bestMoviesPage2 = [response.results[0].url, response.results[1].url, response.results[2].url], 
+
+			// Make the list
+		    listElement = document.createElement('ul'),
+		    // Set up a loop that goes through the items in listItems one at a time
+		    numberOfBestMoviesPage2 = bestMoviesPage2.length,
+		    bestMovie,
+		    i; 
+		    // for (let bestMovieUrlPage2 of bestMoviesPage2)
+		    for (var i = 0; i < numberOfBestMoviesPage2; ++i) {}
 		}
 	}
 })
