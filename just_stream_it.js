0f64ddd7864ac77c4f393ea2bad9db7452c51d47
Kmenguete <menguetekevin@gmail.com>
2022-02-25 10:04:20 +0100

    still trying to make my carousel work

--- just_stream_it.js
+++ just_stream_it.js
@@ -403,27 +403,23 @@ loadTopRatedMovies(
 
 // Now I will make my carousel buttons work
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["best_movies", "Romance", "Animation", "Crime"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("modal_info");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var x = document.getElementById(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
-  
+  x[slideIndex[no]-1].style.display = "block";
 }
