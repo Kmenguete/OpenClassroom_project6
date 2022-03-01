850111601e96b53335c586e709415a5511a22d12
Kmenguete <menguetekevin@gmail.com>
2022-03-01 11:59:44 +0100

    why my for loop does not work?

--- just_stream_it.js
+++ just_stream_it.js
@@ -413,13 +413,18 @@ let items = [best_movies_items, romance_items, animation_items, crime_items];
 
 function goTothePreviousItem (no) {
 	let category = items[no];
-	category_item = document.getElementsByTagName("li");
-	category_item.style.transform = "translateX(-350px)";
+	for (let i = 0; i < category.length; i++) {
+		let category_item = category[i]
+		category_item.style.transform = "translateX(-350px)"; 
+	}
 
 };
 
 function goTotheNextItem (no) {
 	let category = items[no];
-	category_item = document.getElementsByTagName("li");
-	category_item.style.transform = "translateX(350px)";
+	for (let i = 0; i < category.length; i++) {
+		let category_item = category[i]
+		category_item.style.transform = "translateX(350px)"; 
+	}
+
 }; 
